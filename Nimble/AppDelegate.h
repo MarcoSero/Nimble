94470923386c822344e3c96376cb6bfbd9a291db
Marco Sero <marco@marcosero.com>
2013-07-10 22:51:08 +0100

    Initial Commit

---
+++ Nimble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Nimble
+//
+//  Created by Marco Sero on 10/07/13.
+//  Copyright (c) 2013 Marco Sero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nimble/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Nimble
+//
+//  Created by Marco Sero on 10/07/13.
+//  Copyright (c) 2013 Marco Sero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
